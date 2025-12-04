a6037ca00f880cd02c89ee796fe1a632d257f85d
hoc.lnq6 <hoc.lnq6@gmail.com>
2025-12-04 18:57:27 +0000

    ignore password

--- openstreetmap_app/src/services/firebase.ts
+++ openstreetmap_app/src/services/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA25WGyDeW9JL0FwHgmn6zyz_XH7G7k4h4",
-  authDomain: "tdtt-web.firebaseapp.com",
-  projectId: "tdtt-web",
-  storageBucket: "tdtt-web.firebasestorage.app",
-  messagingSenderId: "623381305792",
-  appId: "1:623381305792:web:a153231bf99b24be0f9025",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
   measurementId: "G-KJ29DH752J"
 };
 
